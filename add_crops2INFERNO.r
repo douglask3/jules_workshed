ee95c9a516cc70d9cf4d8d0cf8e3e767829a52e0
Douglas Kelley <douglas.i.kelley@gmail.com>
2018-02-20 23:13:18 +0000

    opening unscaling fire by pft for INERNFO, scaling by crop, and setting up a sudo nls for oprimziation nof new paparemter

--- add_crops2INFERNO.r
+++ add_crops2INFERNO.r
@@ -5,6 +5,7 @@ library(raster)
 library(rasterExtras)
 source("libs/logit.r")
 source("libs/converting_between_pacific_centric_and_regular_grid.r")
+source("libs/openInferno.r")
 
 cropFile = '../LimFIRE/outputs/cropland2000-2014.nc'
 modfFile = '../fireMIPbenchmarking/data/ModelOutputs/Inferno/Inferno_S1_burntArea.nc'
@@ -14,24 +15,42 @@ obsfFile = '../LimFIRE/outputs/fire2000-2014.nc'
 x0 = -15.41
 k  = -0.08
 
-modLayers = (3767 - 13*12 + 1):3767
-modLayers = (3767 - 11):3767
+modLayers  = (3767 - 13*12 + 1):3767
+modLayers  = (3767 - 11):3767
 
-scaling   = c(0.6, 1.4, 1.2)
+scaling_in = c(0.6, 0.6, 0.6, 0.6, 0.6, 1.4, 1.4, 1.2, 1.2)
 
 ###########################
 ## open Data             ##
 ###########################
-crop = brick(cropFile)	
 
-mod = mean(openInferno.fire(modfFile, modvFile))
+mod = mod0 = openInferno.fire(modfFile, modvFile, scaling_in)
 mod = convert_pacific_centric_2_regular(mod)
 
 obs = mean(brick(obsfFile))
 obs = raster::resample(obs, mod)
 
-fpc = brick('data/qrparm.veg.frac.nc')
+fpc = brick('data/qrparm.veg.frac.nc')[[1:9]]
 fpc = convert_pacific_centric_2_regular(fpc)
 
+crop = brick(cropFile)[[1:12]]
+crop = raster::resample(crop, mod)	
+
 ## calculate suppression
-suppression = layer.apply(crop, logit1At0, x0, k)
+suppression = mean(layer.apply(crop, logit1At0, x0, k)) ## Want to apply to each month maybe?
+mod = mod * suppression
+
+mask = !is.na(sum(fpc))
+
+vmod = mod[mask]
+vobs = obs[mask]
+vfpc = fpc[mask]
+
+makeFire <- function(fire, fpc, ...) {
+	fire = sweep(fire, 2, c(...), '*')
+	fire = fire * fpc
+	return(fire)
+ }
+ 
+nls(obs ~ makeFire(fire, fpc, ...
+	
--- libs/openInferno.r
+++ libs/openInferno.r
@@ -1,15 +1,25 @@
-openInferno.fire <- function(fireFile, vegFile) {
+openInferno.fire <- function(fireFile, vegFile, deconstruct = NULL) {
 	byLayer <- function(l, mnth) {
+		
 		fire = brick(fireFile, level = l)[[mnth]]
 		veg  = brick( vegFile, level = l)[[mnth]]
-		return(fire * veg)
+		if (is.null(deconstruct)) fire = fire * veg else fire = fire / deconstruct[l]
+		return(fire)
 	}
 	byLevel <- function(...) {
 		fire = layer.apply(1:9, byLayer, ...)
-		fire = sum(fire) * 60 * 60 * 24 * 30
+		if (is.null(deconstruct)) fire = sum(fire) 
+		fire = fire * 60 * 60 * 24 * 30
 		return(fire)
 	}
 	fire = layer.apply(modLayers, byLevel)
+	if (!is.null(deconstruct)) {
+		meanLayer <- function(l) {
+			index = seq(l, nlayers(fire), 9)
+			mean(fire[[index]])
+		}
+		fire = layer.apply(1:9, meanLayer)
+	}
 	return(fire)
 }
 
